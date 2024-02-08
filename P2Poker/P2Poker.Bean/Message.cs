7ccec63dd2581be987cfd29c69fb730d15d03915
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2024-02-08 11:59:44 -0300

    up

--- P2Poker/P2Poker.Bean/Message.cs
+++ P2Poker/P2Poker.Bean/Message.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using Newtonsoft.Json;
 using P2Poker.Enums;
@@ -21,6 +22,31 @@ public class Message
         var obj = DeserializeFromString<Msg>(_string);
         NotFoundException.ThrowIfNull(obj, $"{obj} Is not Deserialize From String");
         processDataCallback(obj.requestCode, obj.actionCode, obj._string);
+    }
+    [Obsolete("Obsolete")]
+    public byte[] ConverteObjectEmByte(object? obj)
+    {
+        byte[] serializedBytes;
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            binaryFormatter.Serialize(memoryStream, obj);
+            serializedBytes = memoryStream.ToArray();
+            return serializedBytes;
+        }
+
+        return serializedBytes;
+    }
+
+    [Obsolete("Obsolete")]
+    public T? ConverteByteEmObject<T>(byte[] bytes)
+    {
+        using (MemoryStream memoryStream = new MemoryStream(bytes))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            T obj = (T)binaryFormatter.Deserialize(memoryStream);
+            return obj;
+        }
     }
 
     T? DeserializeFromString<T>(string s)
